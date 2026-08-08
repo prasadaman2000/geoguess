562ce60808d1f9143004523ecd5d609077fc904a
Aman Prasad <prasdaman2000@gmail.com>
2026-08-07 21:31:48 -0700

    one photo of juan

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
+var images = [
+  {
+    "path": "https://lh3.googleusercontent.com/pw/AP1GczPq7M7HaPgue9byqPvZ2EArqMD60KiqtUSFh7UTArvfH04baQzvcm0IS617I45PO4Ro7qWg8HeT93nXejqKgDZnjtxYdP6g_Ihmrq9SpWqh1cawQFE27izh0Y1gZG1poaXdp1Ztay3MDX6Zfh-34JRHhg=w665-h887-s-no-gm?authuser=0",
+    "date": "2021-01-01",
+    "location": {
+      "lat": 120.01,
+      "long": 123.02
+    }
+  }
+]
+
 function hello() {
-  alert("hi");
+  var files = readdirSync('./imgs/');
+  console.log(files);
 }
