748e013e3b876158208b173db9f4faa334176710
Aman Prasad <prasdaman2000@gmail.com>
2026-08-08 00:11:41 -0700

    MVP

--- main.js
+++ main.js
@@ -1,15 +1,305 @@
 var images = [
   {
-    "path": "https://lh3.googleusercontent.com/pw/AP1GczPq7M7HaPgue9byqPvZ2EArqMD60KiqtUSFh7UTArvfH04baQzvcm0IS617I45PO4Ro7qWg8HeT93nXejqKgDZnjtxYdP6g_Ihmrq9SpWqh1cawQFE27izh0Y1gZG1poaXdp1Ztay3MDX6Zfh-34JRHhg=w665-h887-s-no-gm?authuser=0",
-    "date": "2021-01-01",
+    "path": "https://lh3.googleusercontent.com/pw/AP1GczPnlqvNESaehXOukA-NuFy_-pEijT8b9jpGOTSMaeJ247SvV35m1ySwbtlfGm9wcUxxFZkGJNq2BN6mcFc07-Fg3CrV-H_KUXk2BLqCUy96OUTcdS8GFahiF2PZ_Vga44cfyG0WT25_8at2956yFkQXng=w1746-h2328-s-no-gm?authuser=0",
+    "date": new Date("2024-12-12"),
     "location": {
-      "lat": 120.01,
-      "long": 123.02
+      "lat": 37.798,
+      "long": -122.408
+    }
+  },
+  {
+    "path": "https://lh3.googleusercontent.com/pw/AP1GczMP475xNzCMPKNfrqoJ5z27uqnTC140VDgnVJoFWKMdgQtrdCaeDD43bPQcnR9AUq4uE39NPBsTPnpatPgwEjrR5zqt-1tow3PIOFT7J80ibhDUk4NlVgvUMTGgbTkOfWQV3RFXt8Ggrpwgp4qUfiNqVQ=w1118-h1490-s-no-gm?authuser=0",
+    "date": new Date("2026-07-04"),
+    "location": {
+      "lat": - 32.73455850910409,
+      "long": -117.1445100887043
+    }
+  },
+  {
+    "path": "https://lh3.googleusercontent.com/pw/AP1GczOIrPGqkETE2wekG6j8GYKPPmF54T_A1HCQSn7i8Q35scLmXSJTvYRZBG4EAQ37VXQ1HO1z1SX5VX4_UQKCFeXGRdlMqPnlIg0-SKcnMNYOc-F_o3pSy4Mr7fJOPvSrHGVbq9PS3w9p9zhfDjJ6uAQbBg=w1118-h1490-s-no-gm?authuser=0",
+    "date": new Date("2024-03-09"),
+    "location": {
+      "lat": -37.331614682132155,
+      "long": -121.90533721636733
+    }
+  },
+  {
+    "path": "https://lh3.googleusercontent.com/pw/AP1GczPwX5vtlRFKxktHjIZTZjdIrs5MkKBDcWhHvNMfYrYty0EbanRBdLXDRjeqtQWPs_fnscFHTpovZpV205avhF0PIIfFVATzw-4f7O8yGMpgG37E7xd-qT4VyZVOGcbXoSaLfFimrvV27BwKoks4dsEhOA=w1118-h1490-s-no-gm?authuser=0",
+    "date": new Date("2026-07-03"),
+    "location": {
+      "lat": 32.834188300162495,
+      "long": -117.27234646171398
+    }
+  },
+  {
+    "path": "https://lh3.googleusercontent.com/pw/AP1GczP7T9gP33xTY6RGxr_vM1uCQKnuPyWflvFtxS9vQ9LFT4wRuas-y9z1fmbCrhcgcctpPLXYVqdlqmfaAvRicx8W_oiZRdkwLjD9zRBah8qjrhVbb2oUzfxYKLlYWvILRjnr_G_wAiOAmC2YsY5TWT495Q=w1118-h1490-s-no-gm?authuser=0",
+    "date": new Date("2026-06-28"),
+    "location": {
+      "lat": 37.41192113671182,
+      "long": -122.09692136884831
+    }
+  },
+  {
+    "path": "https://lh3.googleusercontent.com/pw/AP1GczOfxK0AheuLsoE_M1D41_XostMHEOFFMb72WgyvUZTatcka10vt4ay0N4VOoxgy-PYtAQPamGosCP8thvD3mpD_7CPXRJ7h6Df6sH_s5jyOyInfuNHVeA_yEHLoLU94J1kd5tyIrS1Z4nYegGab1d-Gbg=w1118-h1490-s-no-gm?authuser=0",
+    "date": new Date("2026-06-19"),
+    "location": {
+      "lat": 37.41192113671182,
+      "long": -122.09692136884831
+    }
+  },
+  {
+    "path": "https://lh3.googleusercontent.com/pw/AP1GczOLZUk99FY1Hjhm4L8_fClvtg8YM4xaZz-3ljInJXPq1HzkOi4hccAA__HxyCSCTw6KwWipekUAwSdEQ-nYHUBVKuhM7Ap-JVeCxev2pSgkQGXyU7nxent8jyYOgav99zkyH0TxclJIh085aDeA7OoKEg=w1118-h1490-s-no-gm?authuser=0",
+    "date": new Date("2026-01-24"),
+    "location": {
+      "lat": 37.41192113671182,
+      "long": -122.09692136884831
+    }
+  },
+
+  {
+    "path": "https://lh3.googleusercontent.com/pw/AP1GczOJrvzPR-w9Qeb2vLGvtnmNubOV-xtxQEoX-RD-3U-t1glAWiy3COlhYDUxIMFxz-R9EsG6G4BUh1STEU94SEzSyeSAH0jnv00HyEMF-sFKmLC-fX2lgCg3mrL5a_REAk8pi4NL9jd3qkf9aOd48-4hsw=w1118-h1490-s-no-gm?authuser=0",
+    "date": new Date("2025-12-03"),
+    "location": {
+      "lat": 37.76990697648476,
+      "long": -122.46612689034122
+    }
+  },
+
+  {
+    "path": "https://lh3.googleusercontent.com/pw/AP1GczNRUaudsP8xVI6d3b-x2JwOhrf5ZbhqcWT-B5O6p35i0FQ3hGWCVkXE9dAfZPY6HH7QQ1fwziHixJAm_-VcfPty_8PKfP1gNuIvWvLd1Bt2wqR_fjPHkhPGCXN2aZXyo3S8_ckhyh7XGwgk11LWpzK3FQ=w1746-h2328-s-no-gm?authuser=0",
+    "date": new Date("2025-03-15"),
+    "location": {
+      "lat": 37.543,
+      "long": -122.299
+    }
+  },
+
+  {
+    "path": "https://lh3.googleusercontent.com/pw/AP1GczOYJEX9AmwI9aoWRn4BkA_PW0HCDQ0P1M_VvCDAp3ezpY-EhdKpfNs_LKPy5z-lOOVi9jusMxSl-Rgl2URHGEo4qQnerO2JZvfpBfTwAT3hXO-3fDhExMAoTYnoQZBWJSDPbBukf5VHbCmuuHiG4p_Kwg=w1746-h2328-s-no-gm?authuser=0",
+    "date": new Date("2025-03-21"),
+    "location": {
+      "lat": 37.353,
+      "long": -121.987
+    }
+  },
+
+  {
+    "path": "https://lh3.googleusercontent.com/pw/AP1GczOYpyrdnPN-5gil8tZSVCBZkRNZLW-y_4FGoY4a44SLcZ6-Ij1wwFDR4jB_kE5qFK5A8_HGFiIHSuH6l182q79Yiwf4tpEmn2xy6NREOEIO4XS8InDGEldSt5FUUR42n1HP_Ak4S6PFSJmJSaQvSZLzCw=w1746-h2328-s-no-gm?authuser=0",
+    "date": new Date("2025-05-25"),
+    "location": {
+      "lat": 38.291,
+      "long": -122.440
+    }
+  },
+
+  {
+    "path": "https://lh3.googleusercontent.com/pw/AP1GczMK3FcLIVHdl9HfCoyTLg1YjvsR9pH_-vTH1AX_Barht5v-SXkhlDXkX5T5fyRT7uQqDCBQH1RWOsEAEgKy9rSuLbY8Im1Wji-M9jq_UR-TV7qlgt0Q1FhPjv-9KqGbQiHjdJRt41-y94kMS_dRLLBSLQ=w1746-h2328-s-no-gm?authuser=0",
+    "date": new Date("2025-05-31"),
+    "location": {
+      "lat": 37.394,
+      "long": -122.080
+    }
+  },
+
+  {
+    "path": "https://lh3.googleusercontent.com/pw/AP1GczPVweOHrBq0m3HSbyPj2Z_C7OwcRVjN4N0vnhI-h7EUqH2wERnrqmK7G5c83Lhng1nlXio6fmckVwVUzWtvUlzKJjZBtUlQbNj6RK5mM85TKfUzKo6fSPW9bcth4tqXeDxnA5ZdOdrhjwvZCCybRSvJDA=w1746-h2328-s-no-gm?authuser=0",
+    "date": new Date("2025-06-07"),
+    "location": {
+      "lat": 37.782,
+      "long": -122.513
+    }
+  },
+
+  {
+    "path": "https://lh3.googleusercontent.com/pw/AP1GczPFVOMLgJPWZiW8SgXm8OQGSrfAjeeblNjsDM5M8Hzc2XJm-rSztg37Lb2021Wre_W_8h7jF9v3PGTusXnC2ljIP5TXxcwgP_zYTL3RwlncMn_i_A_nNjoo5pBBhVDIW8KOXmFdOQGss1kEC9ICtR-X5Q=w1746-h2328-s-no-gm?authuser=0",
+    "date": new Date("2025-06-14"),
+    "location": {
+      "lat": 37.329,
+      "long": -122.380
+    }
+  },
+
+
+  {
+    "path": "https://lh3.googleusercontent.com/pw/AP1GczPZVuBTSN44h1ZEeUq2Y5sq44z_vXAyC8ge70-HjTwoQX9Qh9i8PWtMJGCc4m1y16cH7ZX85lr7-2miw0XSmQFAYZssawff6J_2kwvhjkwjQ28mgQMsZa02d9YxyJlYDl9g-pNAlnK6JHm1eXsVUjdP6Q=w1118-h1490-s-no-gm?authuser=0",
+    "date": new Date("2025-07-02"),
+    "location": {
+      "lat": 36.262611713700416,
+      "long": -121.79752978300455
+    }
+  },
+
+  {
+    "path": "https://lh3.googleusercontent.com/pw/AP1GczMWWDIs1Fxcjpmp2jzrW6Q06eA2gU-gPOMzNb_NzXUpVivjwtukyIcChqeN0rAJ0EupNKiclk7vRzIPOFLVTFq-nSK27F_tGr9H07nX7a4YafJiwUYIIxMoFfsvpnyEmWhTV6nWJA0PSLP60qL4hoCSZA=w2000-h921-s-no-gm?authuser=0",
+    "date": new Date("2024-08-14"),
+    "location": {
+      "lat": 21.39275646211461,
+      "long": -157.71524405687944
+    }
+  },
+
+  {
+    "path": "https://lh3.googleusercontent.com/pw/AP1GczOBBmns4vxZu0rvXK7VYPRWMcuFKCJw6xNkrdxUBpatzt3gT9ED2-X0lhzmGTee4Pu1feJclIJ4E70iVL6Dxd2pWKeqCYFUbuRTTQz-1-kX3EUDS_r53h2yPW5a_CfjTLchMrObIxYUxWDl0Bo1Ub1-gQ=w1986-h1490-s-no-gm?authuser=0",
+    "date": new Date("2023-10-01"),
+    "location": {
+      "lat": 47.62709348908796,
+      "long": -122.33637435443137
+    }
+  },
+
+  {
+    "path": "https://lh3.googleusercontent.com/pw/AP1GczO5M4G5IV3bfVK39ajvPf9HiUowuOOl3v_B6-pSZWWE_2ERs733W6c1v2DD_LW-bhwEQFWweiuZroP8mJRsRSgbQFCS-oa0Phud6Yxc2oy5xih8HbH8aZzA5eTbrRE2SKO4_UleBYh6Zfu79ICE6HPRdg=w1986-h1490-s-no-gm?authuser=0",
+    "date": new Date("2024-01-27"),
+    "location": {
+      "lat": 37.54891567346499,
+      "long": -121.99591586462944
+    }
+  },
+
+  {
+    "path": "https://lh3.googleusercontent.com/pw/AP1GczPMkIW-jNd8stZREhQyDlXQGOmpy_czrYVDp30s-MA3QAKQ5HJo0_XAQZYuhZwCYtTOWy3BsWOuhTtbLSOn0FAqCBNycCy9FqmfAk6DQmHyQASKy034umQnbYr6R1vVaEDbF5tCzgIFA8EWc-POVXj6GA=w974-h1490-s-no-gm?authuser=0",
+    "date": new Date("2024-08-01"),
+    "location": {
+      "lat": 25.446826328278036,
+      "long": -102.18109998921864
+    }
+  },
+
+  {
+    "path": "https://lh3.googleusercontent.com/pw/AP1GczORkzigqMfIpTOYN5xlQRPSwTQB46YxJxPNJ3SzPsEqopjkTXGIfTFGPnX4Zezrjf34Kad_4vSjMCxt-AeLe4WcEiUEsXok6eoAgIMNEiJjTs0CU9dwTfhUmvnv7fDOGnw5Q_Im3fivbA5TOGjj4H0cjw=w1986-h1490-s-no-gm?authuser=0",
+    "date": new Date("2024-10-14"),
+    "location": {
+      "lat": 35.69614507290125,
+      "long": 139.70112818754345
+    }
+  },
+
+  {
+    "path": "https://lh3.googleusercontent.com/pw/AP1GczPWT4XOTfMCfuOnva6kiw1bAir0T9rYs0mAFCuYFtSGRwqrf6KpDupVQ43eExOI3EoZh0FZpm26VOva2Omf1sQ2gccNQeQn7Wwa7g4Of83z2vT1a8oNnq0n6GJYbzwPxsls9q0j8---bfxX89u3SG_bfQ=w1118-h1490-s-no-gm?authuser=0",
+    "date": new Date("2025-01-18"),
+    "location": {
+      "lat": 37.806198902393255,
+      "long": -122.42302410522582
+    }
+  },
+
+  {
+    "path": "https://lh3.googleusercontent.com/pw/AP1GczO82FXFp53oHY5vjQRNgaOOp2WesM9a3LlyJNMmHehiGQiHBLrRc1yeCiQZXNTSRw0lfREs6h9ZmMEV-aBaVFZM5ux1sXI1BaU6khj6il-3iA1gVluha4-Cj9jrTGsIsIfGpDQN-z5_vjo3MYZKthCBow=w1118-h1490-s-no-gm?authuser=0",
+    "date": new Date("2026-02-26"),
+    "location": {
+      "lat": 37.80431972035082,
+      "long": -122.43732350601186
+    }
+  },
+
+  {
+    "path": "https://lh3.googleusercontent.com/pw/AP1GczMo0g8FWQkmutTSP2m3OkBb6cBY99jJFBML-6SF8WIE6GcqL35SxBJRREkPzm8X7co-wsPQk4fEZQGYEqwUYJtWKwPsOkZN3gKx5C9Nr_JGmRoSKxKdkoJMQS2ieigHwmEPtwQjTkchg251bGeKNJ42vQ=w1746-h2328-s-no-gm?authuser=0",
+    "date": new Date("2025-10-11"),
+    "location": {
+      "lat": 37.323,
+      "long": -122.035
+    }
+  },
+  {
+    "path": "https://lh3.googleusercontent.com/pw/AP1GczPq7M7HaPgue9byqPvZ2EArqMD60KiqtUSFh7UTArvfH04baQzvcm0IS617I45PO4Ro7qWg8HeT93nXejqKgDZnjtxYdP6g_Ihmrq9SpWqh1cawQFE27izh0Y1gZG1poaXdp1Ztay3MDX6Zfh-34JRHhg=w665-h887-s-no-gm?authuser=0",
+    "date": new Date("2025-08-12"),
+    "location": {
+      "lat": 37.423458148808685,
+      "long": -122.06666867333563
+    }
+  },
+  {
+    "path": "https://lh3.googleusercontent.com/pw/AP1GczMkDB_PpJsAnFpXHWoqm0UYRF_rnLzMyw9VzRWaOdG0PB6ecSSCaSBvKh3_dqMT842OBnwlxB7SjYOXqXv4cvPA4sphZPo5STjLHhRBTMTel2qRG-5PZyDqMxr0VGyeuqOYslYoeJFsaBRr4eWz1Lkfbg=w665-h887-s-no-gm?authuser=0",
+    "date": new Date("2025-09-20"),
+    "location": {
+      "lat": 36.371446951213265,
+      "long": -121.9016913075358
     }
   }
 ]
 
-function hello() {
-  var files = readdirSync('./imgs/');
-  console.log(files);
+
+var kEarthRadius = 3959
+
+var currImage = {}
+var map = L.map('map').setView([37.423458148808685, -122.06666867333563], 10);
+const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  edgeBufferTiles: 2,
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+var lastClick = {
+  "lat": 0,
+  "long": 0,
+};
+var marker = "";
+function onMapClick(e) {
+  lastClick["lat"] = e.latlng["lat"];
+  lastClick["long"] = e.latlng["lng"];
+  if (marker != "") {
+    marker.remove();
+  }
+  marker = L.marker([lastClick["lat"], lastClick["long"]]).addTo(map);
+}
+map.on('click', onMapClick);
+
+function start() {
+  var idx = Math.trunc(Math.random() * images.length);
+  currImage = images[idx];
+  document.getElementById("thePicture").setAttribute("src", currImage["path"]);
+  document.getElementById("startButton").setAttribute("hidden", "true");
+  document.getElementById("map").removeAttribute("hidden");
+  document.getElementById("submitButton").removeAttribute("hidden");
+
+}
+
+function dayDiff() {
+  var enteredDate = new Date(document.getElementById("dateOfImage").value);
+  var diffMs = Math.abs(enteredDate - currImage["date"]);
+  return (diffMs / (1000 * 60 * 60 * 24));
+}
+
+function latLongToRadians(latlong) {
+  return {
+    "lat": (latlong["lat"] * Math.PI) / 180,
+    "long": (latlong["long"] * Math.PI) / 180,
+  }
+}
+
+function latLongToXYZCoordinates(latlong) {
+  return {
+    "x": kEarthRadius * Math.sin(latlong["lat"]) * Math.cos(latlong["long"]),
+    "y": kEarthRadius * Math.sin(latlong["lat"]) * Math.sin(latlong["long"]),
+    "z": kEarthRadius * Math.cos(latlong["lat"])
+  }
+}
+
+function distDiff() {
+  console.log(lastClick)
+  console.log(currImage["location"])
+
+  var guessCoordinates = latLongToXYZCoordinates(latLongToRadians(lastClick));
+  var actualCoordinates = latLongToXYZCoordinates(latLongToRadians(currImage["location"]));
+
+  console.log(guessCoordinates)
+  console.log(actualCoordinates)
+
+  var deltas = {
+    "x": guessCoordinates["x"] - actualCoordinates["x"],
+    "y": guessCoordinates["y"] - actualCoordinates["y"],
+    "z": guessCoordinates["z"] - actualCoordinates["z"],
+  }
+
+  console.log(deltas);
+
+  return Math.sqrt(deltas["x"] * deltas["x"] + deltas["y"] * deltas["y"] + deltas["z"] * deltas["z"]);
+}
+
+function submit() {
+  var differenceInDays = dayDiff();
+  var differenceMiles = distDiff();
+  alert("you were off by " + differenceInDays + " days and " + differenceMiles + " miles.");
+  document.getElementById("submitButton").setAttribute("hidden", "true");
+  document.getElementById("startButton").removeAttribute("hidden");
+  document.getElementById("map").setAttribute("hidden", "true");
 }
